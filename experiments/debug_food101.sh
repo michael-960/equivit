1fb64cb9d279c29c6c08ca46fd2e55503a3df7f0
Chih-Chun <ch.wang96029@gmail.com>
2026-05-20 07:58:06 +0200

    changed debug script for food101

--- experiments/debug_food101.sh
+++ experiments/debug_food101.sh
@@ -4,7 +4,7 @@ set -e
 source ./env.sh
 
 
-subgroup=C1
+subgroup=C4
 depth=4
 compile=false
 
@@ -21,18 +21,18 @@ args=(
 	+trainer.limit_train_batches=1.0
 	+trainer.limit_val_batches=10
 
-	model=octic/"$subgroup"/a/extratiny
+	model=octic/"$subgroup"/a/small
 	model.backbone.config.depth="$depth"
-	model.backbone.config.tokenizer_config.patch_size=32
-	# +model.head.drop_rate=0.
-	# +model.backbone.config.transformer_block_config.attn_drop=0.
-	# +model.backbone.config.transformer_block_config.drop_path=0.05
+	model.backbone.config.tokenizer_config.patch_size=16
+	+model.head.drop_rate=0.3
+	+model.backbone.config.transformer_block_config.attn_drop=0.1
+	+model.backbone.config.transformer_block_config.drop_path=0.1
 
 	compile="$compile"
 
 	optimizer.lr=0.0001
 
-	data=food101
+	data=food101_256
 	loss_fn._target_=torch.nn.CrossEntropyLoss
 
 	data.train_loader.batch_size=64
